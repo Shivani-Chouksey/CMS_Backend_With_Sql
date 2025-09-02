c3c6ffe622071f80ab374a3d166bc3fd024b8c67
Shivani-Chouksey <122255940+Shivani-Chouksey@users.noreply.github.com>
2025-09-02 22:26:10 +0530

    Customize multer upload middleware

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,5 +1,6 @@
 import multer from 'multer';
-
+import path from 'path';
+import fs from 'fs';
 
 // save files inside existing project directory
 const storage = multer.diskStorage(
@@ -22,3 +23,45 @@ const storage = multer.diskStorage(
 
 export const upload = multer({ storage: storage });
 
+// Create storage dynamically
+const createStorage = (folderName) => {
+  return multer.diskStorage({
+    destination: (req, file, cb) => {
+      const uploadPath = path.join("public", folderName);
+
+      // Ensure folder exists
+      if (!fs.existsSync(uploadPath)) {
+        fs.mkdirSync(uploadPath, { recursive: true });
+      }
+
+      cb(null, uploadPath);
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + "-" + (req.body?.title || "file");
+      const ext = path.extname(file.originalname);
+      cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+    },
+  });
+};
+
+// Reusable middleware generator
+export const createUpload = (folderName, options = {}) => {
+  const limits = {
+    fileSize: options.fileSize || 5 * 1024 * 1024, // default 5MB
+  };
+
+  const fileFilter = (req, file, cb) => {
+    if (options.allowedTypes) {
+      if (!options.allowedTypes.includes(file.mimetype)) {
+        return cb(new Error("Invalid file type"), false);
+      }
+    }
+    cb(null, true);
+  };
+
+  return multer({
+    storage: createStorage(folderName),
+    limits,
+    fileFilter,
+  });
+};
--- routes/company.routes.js
+++ routes/company.routes.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import { CreateCompany } from '../controllers/company.controllers.js';
 import { Is_Super_Admin } from '../middleware/jwt/check-cms-user-auth.middleware.js';
-import { upload } from '../middleware/multer.middleware.js';
-const router=express.Router();
+import { createUpload, upload } from '../middleware/multer.middleware.js';
+const router = express.Router();
 
-router.post("/create",Is_Super_Admin,upload.single('company_logo'),CreateCompany)
-
+// router.post("/create",Is_Super_Admin,upload.single('company_logo'),CreateCompany)
+// News uploads
+router.post(
+    "/create",
+    // Is_Super_Admin,
+    createUpload("company", {
+        fileSize: 5 * 1024 * 1024, // 2MB
+        allowedTypes: ["image/jpeg", "image/png",'application/pdf'],
+    }).single("company_logo"),
+    CreateCompany
+);
 
 
 export default router 
