43b3e78fe04af75e50dd2b1b4d7dea6be2aeaf1e
chouksey-shivani <shivani.chouksey@digitalsalt.in>
2025-09-14 19:09:26 +0530

    Add Morgan logger and Make logs file

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ import express from 'express';
 import 'dotenv/config';
 import All_API_Routes from './routes/index.js';
 import { DbConnection } from './config/db-connection.js';
+import morgan from 'morgan';
+import path from 'node:path';
+import fs from 'node:fs';
 
 const app = express();
 app.use(express.json());
+
+// ensure log directory exists
+const logDirectory = path.join(process.cwd(), 'logs');
+if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory)
+}
+
+//create a write stream 
+const accessLogStream = fs.createWriteStream(path.join(logDirectory, "access.log"), { flags: 'a' });
+
+//setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
+app.use(morgan('dev'))
+
 
 // All Routes
 app.use('/api/v1', All_API_Routes);
